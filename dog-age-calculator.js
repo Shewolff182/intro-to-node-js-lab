42c3f36b04fad7684e8878bb43ccfe62b3a3eadf
shan wolff <shanwolff182@gmail.com>
2023-11-13 17:07:35 -0500

    initial commit

---
+++ dog-age-calculator.js
@@ -0,0 +1,8 @@
+ let dogName = process.argv[2];
+ let humanAge = process.argv[3];
+
+// let humanYear1 = 15;
+// let humanYear2 = humanYear1 + 9;
+// let humanYearsAdditional = humanYear2 + 5;
+
+console.log("Your dog, " + dogName " , is " + humanAge + " years old, but is " dogYears + " years old in dog years");
