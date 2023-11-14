b04592098c5540322f22a0d8a393f09d511cdece
shan wolff <shanwolff182@gmail.com>
2023-11-14 13:13:17 -0500

    in progress

--- dog-age-calculator.js
+++ dog-age-calculator.js
@@ -4,5 +4,15 @@
 // let humanYear1 = 15;
 // let humanYear2 = humanYear1 + 9;
 // let humanYearsAdditional = humanYear2 + 5;
+const humanYears = parseInt(answer);
+let dogYears;
+
+if (humanYears <= 1) {
+    dogYears = 15;
+} else if (humanYears === 2) {
+    dogYears = 24;
+} else {
+    dogYears = 24 + (humanYears - 2) * 5;
+}
 
 console.log("Your dog, " + dogName " , is " + humanAge + " years old, but is " dogYears + " years old in dog years");
